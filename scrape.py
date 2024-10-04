bff9a33c515e7669529bd7ef12299013ec9897f6
Alex Long <alexmasonlong@gmail.com>
2024-10-03 21:48:08 -0400

    Add missing session

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 import os
 from requests_html import HTMLSession
 import csv
+session = HTMLSession()
 
 def extractStudent(studentEl):
     title = studentEl.find('.directory-list-item__title', first=True).text
