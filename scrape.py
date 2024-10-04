b6a462da7e6388e3a0ff339e55077c03c55a3d13
Alex Long <alexmasonlong@gmail.com>
2024-10-03 21:42:07 -0400

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,23 +1,6 @@
 import os
 from requests_html import HTMLSession
 import csv
-# ImportError: lxml.html.clean module is now a separate project lxml_html_clean.
-# Install lxml[html_clean] or lxml_html_clean directly.
-# from lxml.html.clean import clean_html
-session = HTMLSession()
-r = session.get('https://drexel.edu/Search?type=student&q=a&page=1&app=directory')
-r.html.render()
-
-# this is what we're trying to scrape:
-# 
-# <div class="directory-list-item__body"><div class="directory-list-item__title is-visible"><span>Aho, Layton, Electrical Engineering, College of Engineering</span></div><div class="directory-list-item__info"><div class="directory-list-item__contact"><div class="directory-list-item__contact-item"><a class="icon-link" href="mailto:layton.aho@drexel.edu"><span class="icon-link__icon"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24" height="24" viewBox="0 0 24 24" role="img" focusable="false">
-#   <g fill-rule="evenodd">
-#     <path d="M20,4 L4,4 C2.9,4 2.01,4.9 2.01,6 L2,18 C2,19.1 2.9,20 4,20 L20,20 C21.1,20 22,19.1 22,18 L22,6 C22,4.9 21.1,4 20,4 L20,4 Z M20,8 L12,13 L4,8 L4,6 L12,11 L20,6 L20,8 L20,8 Z"></path>
-#   </g>
-# </svg>
-# </span><span class="icon-link__text">layton.aho@drexel.edu</span></a></div></div><div class="directory-list-item__location"></div></div></div>
-
-# we want to extract the name, department, college, and email address from this div
 
 student = r.html.find('.directory-list-item__body')
 
